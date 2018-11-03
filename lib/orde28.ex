7d66530eaeda4fe9ae03582466abc713807f8691
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-11-03 15:35:17 +0900

    initial commit

---
+++ lib/orde28.ex
@@ -0,0 +1,18 @@
+defmodule Orde28 do
+  @moduledoc """
+  Documentation for Orde28.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Orde28.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/orde28_test.exs
@@ -0,0 +1,8 @@
+defmodule Orde28Test do
+  use ExUnit.Case
+  doctest Orde28
+
+  test "greets the world" do
+    assert Orde28.hello() == :world
+  end
+end
